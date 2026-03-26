480f4a795281009cebcf887155bfc571eefc8446
Patrick Häcker <patrick.haecker@bosch.com>
2026-03-26 05:15:52 +0100

    Support submodules

--- src/UseAll.jl
+++ src/UseAll.jl
@@ -4,17 +4,22 @@ export @useall
 
 # Use `@useall MyPackage` to simplify debugging from the command line.
 # TODO: It would be cool if this could register in Revise.jl such that if a symbol is added to the package, revise would also make it automatically available.
-# TODO: Implement support for a module which is not itself a package, e.g. Base.Iterators
 macro useall(exs...)
     isempty(exs) && throw(ArgumentError("@useall requires at least one module name, e.g. `@useall MyPackage`"))
     stmts = Expr[]
     for ex in exs
-        @eval __module__ using $ex
+        # Only run `using` for top-level packages; submodules (e.g. Base.Iterators) are already loaded.
+        ex isa Symbol && @eval __module__ using $ex
         m = @eval __module__ $ex
-        push!(stmts, Expr(:using, Expr(:(:), Expr(:., ex), usefulnames(m, __module__)...)))
+        modpath = Expr(:., _splitmodpath(ex)...)
+        push!(stmts, Expr(:using, Expr(:(:), modpath, usefulnames(m, __module__)...)))
     end
     Expr(:escape, Expr(:block, stmts...))
 end
+
+# Convert a module expression to path components: :Foo → (:Foo,), :(Base.Iterators) → (:Base, :Iterators)
+_splitmodpath(s::Symbol) = (s,)
+_splitmodpath(ex::Expr) = (_splitmodpath(ex.args[1])..., ex.args[2].value)
 
 function usefulnames(m_from::Module, m_into::Module=Main)
     # Use FQN for `Base.names` to also work if there is a global variable `names` defined in `Main`.
